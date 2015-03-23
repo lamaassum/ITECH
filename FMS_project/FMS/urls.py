3a6866e9c10e3ecc282f0205b31682edd1e5276f
Lama <assum.lama@gmail.com>
2015-03-23 05:38:40 +0000

    dynamic profile view fixed + Edit Profile (not complete)

--- FMS_project/FMS/urls.py
+++ FMS_project/FMS/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
     #url(r'^ajax_search/', include('ajax_search.urls')),
     url(r'^my_profile/$', views.my_profile, name='my_profile'),
-    url(r'^(?P<user_name_slug>[\w\-]+)/$', views.profile, name='profile'),)
+    url(r'^(?P<user_name_slug>[\w\-]+)/$', views.profile, name='profile'),
+    url(r'^profile_form/$', views.profile_form, name='profile_form'),)
 
