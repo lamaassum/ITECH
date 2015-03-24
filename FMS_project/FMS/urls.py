00859f7c1a2155e6b0460aeb98f22200299f05fd
Lama <assum.lama@gmail.com>
2015-03-24 17:59:30 +0000

    less hard coded

--- FMS_project/FMS/urls.py
+++ FMS_project/FMS/urls.py
@@ -2,11 +2,6 @@ from django.conf.urls import patterns, url, include
 from FMS import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name='index'),
-    url(r'^advanced_search/$', views.issues_search, name='advanced_student_search_form'),
-    url(r'^results/$', views.advanced_search, name='advanced_search'),
-    url(r'^my_profile/$', views.my_profile, name='my_profile'),
-    url(r'^profile_form/$', views.profile_form, name='profile_form'),
-    url(r'^(?P<user_name_slug>[\w\-]+)/$', views.profile, name='profile'),
-    url(r'^favorite_supervisor/$', views.favorite_supervisor, name='favorite_supervisor'),
+
+
     )
--- FMS_project/FMS_project/urls.py
+++ FMS_project/FMS_project/urls.py
@@ -7,12 +7,16 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'FMS_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', views.index, name='index'),
+    url(r'^advanced_search/$', views.issues_search, name='advanced_student_search_form'),
+    url(r'^results/$', views.advanced_search, name='advanced_search'),
+    url(r'^my_profile/$', views.my_profile, name='my_profile'),
+    url(r'^profile_form/$', views.profile_form, name='profile_form'),
+    url(r'^favorite_supervisor/$', views.favorite_supervisor, name='favorite_supervisor'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^FMS/', include('FMS.urls')),
     (r'^accounts/', include('registration.backends.default.urls')),
     url(r'^/$', views.search, name='search_for_something'),
-
+    url(r'^(?P<user_name_slug>[\w\-]+)/$', views.profile, name='profile'),
     )
     
 if settings.DEBUG:
